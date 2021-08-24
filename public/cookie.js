b59a3bf5b3e7082bffa58bc0840432af60630cde
Ioannis Chronakis <ioannis.chronakis@deontics.com>
2021-08-24 15:24:18 +0100

    Sorted out the public page

---
+++ public/cookie.js
@@ -0,0 +1,15 @@
+function setCookie(key, value, expiry) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (expiry * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+function eraseCookie(key) {
+    var keyValue = getCookie(key);
+    setCookie(key, keyValue, '-1');
+}
